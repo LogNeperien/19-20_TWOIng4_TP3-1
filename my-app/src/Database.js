a22f522c7227d40d64c3a0974bb2691b49cd6bc8
unknown <helene.carlier.gubler@gmail.com>
2019-11-14 21:25:29 +0100

    Relation avec le tableau profil

---
+++ my-app/src/Database.js
@@ -0,0 +1,17 @@
+export default [
+	{
+		prenom: "Bob",
+		nom: "Marley",
+		date: "06/02/1945",
+	},
+	{
+		prenom: "Wolfgang",
+		nom: "Mozart",
+		date: "27/01/1756",
+	},
+	{
+		prenom: "Angele",
+		nom: "VanLaeken",
+		date: "02/12/1995",
+	}
+];
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './style.css';
+import Database from './Database';
 
 class AllProfil extends React.Component{
 	
@@ -8,31 +9,50 @@ class AllProfil extends React.Component{
 	render () {	
 		return (
 			<div>
-				<button className="Jeanne" /*onClick={this.props.onClick}*/>
-					coucou
-					//{this.props.value}
+				<button className="Personn" /*onClick={this.props.onClick}*/>
+					Bob
 				</button>
 		
-				<button className="Martine" /*onClick={this.props.onClick}*/>
-					coucou
-				  //{this.props.value}
+				<button className="Personn" /*onClick={this.props.onClick}*/>
+					Amadeus
 				</button>
 				
-				<button className="Claude" /*onClick={this.props.onClick}*/>
-					coucou
-				  //{this.props.value}
+				<button className="Personn" /*onClick={this.props.onClick}*/>
+					Angele
 				</button>
 			</div>
 		  );
 	}
 }
 
+class Profil extends React.Component{
+	
+	constructor(props) {
+		super(props);
+		this.state = {profils : Database, ProfilCo : 0};	
+	}
+	
+	render () {	
+		return (
+			<div>
+			   {this.state.profils[0].nom}
+			</div>
+		  );
+	}
+}
+
+
+
 class AllPage extends React.Component{
+	
 	render() {
         return (
 		  <div className="page">
-			<div className="page-board">
+			<div className="navigation">
 			  <AllProfil/>
+			</div>
+			<div className="info-profil">
+			  <Profil/>
 			</div>
 		  </div>
     );
@@ -46,15 +66,21 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+const profils = [
+	{
+		prenom: "Bob",
+		nom : "Marley",
+		date : "06/02/1945",
+	},
+	{
+		prenom: "Wolfgang",
+		nom : "Mozart",
+		date : "27/01/1756",
+	},
+	{
+		prenom: "Angele",
+		nom : "VanLaeken",
+		date : "02/12/1995",
+	}
+];
 
-
-/*
-class Page extends React.Component {
-  renderAllProfil(i) {
-    return (
-      <Square
-        value={this.props.squares[i]}
-        onClick={() => this.props.onClick(i)}
-      />
-    );
-  }*/
